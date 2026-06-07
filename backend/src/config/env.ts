16843ad91edf0b3b09bf7d52bc69c05172aa1f8b
Emrikur <151168924+Emrikur@users.noreply.github.com>
2026-06-07 13:13:32 +0200

    feat: Add environment configuration files for backend and frontend

---
+++ backend/src/config/env.ts
@@ -0,0 +1,10 @@
+export const getEnv = () => ({
+  DATABASE_URL: process.env.URL_DATABASE,
+  FM_API_KEY: process.env.FM_API_KEY,
+  HOST: process.env.HOST,
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+  FRONTEND_ORIGIN: process.env.FRONTEND_ORIGIN,
+});
---
+++ src/config/env.ts
@@ -0,0 +1,6 @@
+/* export const env = {
+  FM_HOST: process.env.FM_HOST!,
+  FM_DB: process.env.FM_DB!,
+  FM_USERNAME: process.env.FM_USERNAME!,
+  FM_PASSWORD: process.env.FM_PASSWORD!,
+}; */
